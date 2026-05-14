096ed495a8df3b2ee8f73baa59b8f084dfe4b8c3
Facu <gfacu7@gmail.com>
2026-05-14 18:24:10 -0300

    pant

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vasca-v2';
+const CACHE_NAME = 'vasca-v3';
 const ASSETS = ['/', '/index.html', '/styles.css', '/main.js', '/icon-192.png', '/vasca-avatar.jpg'];
 
 self.addEventListener('install', e => {
